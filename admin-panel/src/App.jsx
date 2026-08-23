a08ff34d38f51b4f597706b005c6808f5f71255f
sti <sti.tuto.yt.chanel@gmail.com>
2026-08-23 18:53:12 +0200

    Add files via upload

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, PieChart, Pie, Cell, LineChart, Line } from 'recharts'
 import './App.css'
